1d93bdc43c1452465bf0043f7c039a900fc4f8c6
Evgesha200219 <nurova82@mail.ru>
2021-12-21 23:45:35 +0500

    Добавила  и настроила слайдер swiper

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  observer: true,
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
